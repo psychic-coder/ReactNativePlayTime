76eba5e76d19472573ddf59ec52bc4cb2e6252f9
psychic-coder <rohitganguly450@gmail.com>
2024-06-18 14:03:14 +0530

    PUSHING THE FILES after completion

--- Project08/android/app/src/main/java/com/project08/MainApplication.kt
+++ Project08/android/app/src/main/java/com/project08/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.project05
+package com.project08
 
 import android.app.Application
 import com.facebook.react.PackageList
@@ -10,7 +10,6 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 
 class MainApplication : Application(), ReactApplication {
 
